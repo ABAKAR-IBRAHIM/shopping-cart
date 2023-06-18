0dcc8f0f95218796b7b41815b6fbccd6a99db12a
ABAKAR IBRAHIM <abakarib7@gmail.com>
2023-06-18 19:33:39 +0100

    about edited

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     <div className=" p-32 text-lg font-mono">
       I created the ShopCart store template to showcase my expertise in
       full-stack technologies including Nextjs, Typescript, React, Tailwindcss,
-      Nodejs, and MongoDb among others. This portfolio project is a
+      Nodejs,stripe, and MongoDb among others. This portfolio project is a
       demonstration of my skills and I hope you find it enjoyable and useful to
       work with. Sincerely, Abakar Ibrahim
     </div>
